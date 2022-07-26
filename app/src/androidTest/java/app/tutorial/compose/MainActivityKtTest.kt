0ed922a7ff39d4e55e554e4880d6804437aba06f
Prasanna Anbazhagan <panbazhagan@twilio.com>
2022-07-26 14:27:53 +0530

    save commit

---
+++ app/src/androidTest/java/app/tutorial/compose/MainActivityKtTest.kt
@@ -0,0 +1,21 @@
+package app.tutorial.compose
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import org.junit.Rule
+import org.junit.Test
+
+class MainActivityKtTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun shouldDisplayPreviewView() {
+        composeTestRule.setContent {
+            ReCompositionExample(1) {}
+        }
+        composeTestRule.onNodeWithText("1").assertIsDisplayed()
+    }
+}
--- app/src/main/java/app/tutorial/compose/MainActivity.kt
+++ app/src/main/java/app/tutorial/compose/MainActivity.kt
@@ -1,15 +1,18 @@
 package app.tutorial.compose
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import app.tutorial.compose.ui.theme.ComposeTutorialTheme
@@ -44,17 +47,24 @@ fun RowExample() {
 }
 
 @Composable
-fun ColumnExample() {
+fun ReCompositionExample(clicked: Int, onClicked: () -> Unit) {
     Column {
-        Text("1")
-        Text("2")
+        Button(
+            onClick = { onClicked.invoke() },
+        ) {
+            Text(text = clicked.toString())
+        }
     }
 }
 
+@SuppressLint("UnrememberedMutableState")
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
+    val state = mutableStateOf(1)
     ComposeTutorialTheme {
-        RowExample()
+        ReCompositionExample(state.value, onClicked = {
+            state.value = state.value + 1
+        })
     }
 }
